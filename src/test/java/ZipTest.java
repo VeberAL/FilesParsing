b186c4fcc9d9021190e747e6b5f5cca68183f70b
Alexander Veber <aleo83@rambler.ru>
2024-03-08 17:11:35 +0500

    Update ZipTest.java

--- src/test/java/ZipTest.java
+++ src/test/java/ZipTest.java
@@ -15,18 +15,18 @@ import java.util.zip.ZipInputStream;
 import static com.codeborne.pdftest.assertj.Assertions.assertThat;
 
 public class ZipTest {
-    private static final ClassLoader classLoader = ZipTest.class.getClassLoader();
-    private static final String zipFileName = "Files.zip";
-    private static final String csvFileName = "Jedi.csv";
-    private static final String pdfFileName = "JavaCoffee.pdf";
-    private static final String xlsxFileName = "Fullbody.xlsx";
-    private static final String pdfFileValue = "Кофе – мой друг,";
-    private static final String xlsxFileValue = "УТРОМ";
-    private static final String csvFileValue = "{";
+    private static final ClassLoader CLASS_LOADER = ZipTest.class.getClassLoader();
+    private static final String ZIP_FILE_NAME = "Files.zip";
+    private static final String CSV_FILE_NAME = "Jedi.csv";
+    private static final String PDF_FILE_NAME = "JavaCoffee.pdf";
+    private static final String XLSX_FILE_NAME = "Fullbody.xlsx";
+    private static final String PDF_FILE_VALUE = "Кофе – мой друг,";
+    private static final String XLSX_FILE_VALUE = "УТРОМ";
+    private static final String CSV_FILE_VALUE = "{";
 
 
     private InputStream getInputStreamFileInZipFile(String fileName) throws IOException {
-        ZipInputStream is = new ZipInputStream(Objects.requireNonNull(classLoader.getResourceAsStream(zipFileName)));
+        ZipInputStream is = new ZipInputStream(Objects.requireNonNull(CLASS_LOADER.getResourceAsStream(ZIP_FILE_NAME)));
         ZipEntry entry;
         while ((entry = is.getNextEntry()) != null) {
             if (entry.getName().equals(fileName)) {
@@ -39,30 +39,30 @@ public class ZipTest {
     @Test
     @DisplayName("Поиск значения в CSV в ZIP-архиве")
     void csvFileInZipSearchTest() throws Exception {
-        try (InputStream inputStream = getInputStreamFileInZipFile(csvFileName);
+        try (InputStream inputStream = getInputStreamFileInZipFile(CSV_FILE_NAME);
              CSVReader reader = new CSVReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
             String valueCsv = reader.readNext()[0];
-            assertThat(valueCsv).contains(csvFileValue);
+            assertThat(valueCsv).contains(CSV_FILE_VALUE);
         }
     }
 
     @Test
     @DisplayName("Поиск значения в PDF в ZIP-архиве")
     void pdfFileInZipSearchTest() throws IOException {
-        try (InputStream inputStream = getInputStreamFileInZipFile(pdfFileName)) {
+        try (InputStream inputStream = getInputStreamFileInZipFile(PDF_FILE_NAME)) {
             PDF pdf = new PDF(inputStream);
-            assertThat(pdf).containsExactText(pdfFileValue);
+            assertThat(pdf).containsExactText(PDF_FILE_VALUE);
         }
     }
 
     @Test
     @DisplayName("Поиск значения в XLSX в ZIP-архиве")
     void xlsxFileInZipSearchTest() throws IOException {
-        try (InputStream inputStream = getInputStreamFileInZipFile(xlsxFileName)) {
+        try (InputStream inputStream = getInputStreamFileInZipFile(XLSX_FILE_NAME)) {
             XLS xlsx = new XLS(inputStream);
             String cellValueXlsx = xlsx.excel.getSheetAt(0).getRow(0)
                     .getCell(1).getStringCellValue();
-            assertThat(cellValueXlsx).contains(xlsxFileValue);
+            assertThat(cellValueXlsx).contains(XLSX_FILE_VALUE);
         }
     }
 
